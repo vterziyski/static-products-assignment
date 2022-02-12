428c3bd28174e3a7e3b96f5645595d8641e1322a
vterziyski <vasi0757@stud.kea.dk>
2022-02-12 13:59:13 +0100

    finished

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
+const discount = urlParams.get("discount");
 
 console.log(urlParams);
 
@@ -17,4 +18,18 @@ function showProduct(product){
     document.querySelector(".theproduct img").alt=product.productdisplayname;
     document.querySelector(".price").textContent=product.price;
     document.querySelector(".color").textContent=product.basecolour;
+    document.querySelector(".theproduct .discount").textContent = product.discount;
+    document.querySelector(".price-now").textContent = Math.floor(
+        product.price - product.price * (product.discount / 100)
+      );
+    
+
+    if(product.soldout) {
+        document.querySelector(".descriptionofitem").classList.add("soldout");
+        document.querySelector(".theproduct img").classList.add("soldout");
+        document.querySelector(".product-info").classList.add("soldout");
+    }
+    if (product.discount) {
+        document.querySelector(".descriptionofitem").classList.add("onsale");
+    }
 }
--- productlist.js
+++ productlist.js
@@ -1,41 +1,45 @@
 const urlParams = new URLSearchParams(window.location.search);
 const brandname = urlParams.get("brandname");
+const category = urlParams.get("category");
 
+/* if */
+document.querySelector(".brand-name").textContent = brandname;
 console.log(urlParams);
 
-const url="https://kea-alt-del.dk/t7/api/products?brandname=" + brandname;
-
+const url = "https://kea-alt-del.dk/t7/api/products?brandname=" + brandname;
 
 fetch(url)
-.then (res=>res.json())
-.then(data=>handleProductlist(data));
+  .then((res) => res.json())
+  .then((data) => handleProductlist(data));
 
-
-function handleProductlist(data){
-    data.forEach(showProduct);
+function handleProductlist(data) {
+  data.forEach(showProduct);
 }
 
-
 function showProduct(product) {
-    const template = document.querySelector("#product-template").content;
-    const copy = template.cloneNode(true);
-    copy.querySelector(".description h3").textContent=product.productdisplayname;
-    copy.querySelector("a").setAttribute("href", `product.html?id=${product.id}`);
-    copy.querySelector(".price").textContent=product.price;
-    copy.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  const template = document.querySelector("#product-template").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector(".description h3").textContent =
+    product.productdisplayname;
+  copy.querySelector("a").setAttribute("href", `product.html?id=${product.id}`);
+  copy.querySelector(".price").textContent = product.price;
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 
-    if(product.soldout) {
-        copy.querySelector("section").classList.add("soldout");
-    }
-    if (product.discount) {
-        copy.querySelector("section").classList.add("onsale");
-    }
+  if (product.soldout) {
+    copy.querySelector("section").classList.add("soldout");
+  }
+  if (product.discount) {
+    copy.querySelector("section").classList.add("onsale");
+  }
 
-    copy.querySelector(".price-now").textContent = Math.floor(product.price - (product.price * (product.discount/100)))
-    copy.querySelector(".discount").textContent = product.discount;
-    copy.querySelector(".brand-name").textContent= product.brandname;
+  copy.querySelector(".price-now").textContent = Math.floor(
+    product.price - product.price * (product.discount / 100)
+  );
+  copy.querySelector(".discount").textContent = product.discount;
 
+  const parent = document.querySelector("main");
+  parent.appendChild(copy);
 
-    const parent = document.querySelector("main");
-    parent.appendChild(copy);
 }
